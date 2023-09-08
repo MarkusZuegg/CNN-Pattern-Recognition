3cadac4286477923611920250292c4814249a7cc
MarkusZuegg <markuszuegg@outlook.com>
2023-09-08 13:36:57 +1000

    Works (gpu found)

--- Sb_run.sh
+++ Sb_run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-time=0-00:30:00
+
+#SBATCH --time=0-00:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
-#SBATCH --partition=gpu
+#SBATCH --partition=vgpu
 #SBATCH --job-name="CIFAR10"
 #SBATCH --mail-user=s4744924@student.uq.edu.au
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH —output=output_dir/%j.out
+#SBATCH --output=x%j.out
+#SBATCH --error=x%j.err
 
 module load cuda
 conda activate pytorch2
